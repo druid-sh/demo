43941214f95ae51e86a3470c7c2fc6593cc0ef27
druid-sh <dev@druid.sh>
2025-09-21 17:11:41 +0200

    Fix env name

--- src/app/blog/client.ts
+++ src/app/blog/client.ts
@@ -1,11 +1,11 @@
 import { DruidClient } from "@druid-sh/sdk";
 
-if (!process.env.DRUID_API_KEY) {
+if (!process.env.NEXT_PUBLIC_DRUID_API_KEY) {
   throw new Error("DRUID_API_KEY environment variable not set");
 }
 
 export const druid = new DruidClient({
-  apiKey: process.env.DRUID_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_DRUID_API_KEY,
   basePath: "/blog",
   siteName: "Druid SDK Demo",
   projectId: "cmfs7908t0000z30cjuy8uca0",
