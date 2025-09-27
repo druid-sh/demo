405d9fb6cef57a2c238a6a0e74ff2b5c2734dfbb
druid-sh <dev@druid.sh>
2025-09-27 13:25:52 +0200

    Try static gen

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,10 +10,7 @@ interface BlogPageProps {
   searchParams: Promise<{ page?: string }>;
 }
 
-export async function generateStaticParams() {
-  return [{ page: "1" }, { page: "2" }, { page: "3" }];
-}
-
+export const dynamic = "force-static";
 export const revalidate = 60;
 
 export default async function BlogPage({ searchParams }: BlogPageProps) {
