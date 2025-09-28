9ba118e5eb2da8b90140397ddfe74cd119afb277
druid-sh <dev@druid.sh>
2025-09-28 13:22:49 +0200

    update package.json, pnpm-lock.yaml and client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -8,5 +8,4 @@ export const druid = new DruidClient({
   apiKey: process.env.NEXT_PUBLIC_DRUID_API_KEY,
   basePath: "/blog",
   siteName: "Druid Demo",
-  projectId: "cmfuwykoc0000tr0cilyke3ld",
 });
